43ecdb610a16a9b948513e5a97dfe124c4598cfa
pakxe <pigkill40@naver.com>
2023-02-14 04:43:51 +0900

    style(Typography): framer-motion 적용을 위한 상속 컴포넌트 수정

--- src/component/Typography/Typhography.jsx
+++ src/component/Typography/Typhography.jsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-const Typography = styled.p`
-  font-size: ${(props) => (props.fontSize ? props.fontSize : "16px")};
-  color: ${(props) =>
-    props.color ? props.theme.colors[props.color] : props.theme.colors.black};
+const Typography = styled(motion.p)`
+  font-size: ${(props) => (props.fontSize ? props.fontSize : '16px')};
+  color: ${(props) => (props.color ? props.theme.colors[props.color] : props.theme.colors.black)};
   ${(props) => props.mainTitle && props.theme.font.mainTitle};
   ${(props) => props.smallTitle && props.theme.font.smallTitle};
   ${(props) => props.buttonText && props.theme.font.buttonText};
