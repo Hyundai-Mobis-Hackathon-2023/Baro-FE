c45dd675ba0c32914cf1a772c9cdf539122437ad
sean <sean2337@naver.com>
2023-02-13 14:09:44 +0900

    feat:Slider 기능 추가

--- src/pages/CategoryChoice/component/ThemeKinds.jsx
+++ src/pages/CategoryChoice/component/ThemeKinds.jsx
@@ -1,30 +1,17 @@
 import styled from "styled-components";
-import Typography from "../../../component/Typography/Typhography";
-import Internet from "./INTERNET.png";
-import Margin from "../../../component/Margin/Margin";
 import Button from "../../../component/Button/Button";
+import React, { Component } from "react";
+import SimpleSlider from "./SimpleSlider";
 
-
-const ThemeImgBox = styled.div`
-    width: 240px;
-    height:240px;
-    background-color: #FFFFFF9C;
-    border-radius: 100%;
-    border: 1px solid #D9D9D9;
-    box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.1);
-    box-sizing: border-box;
-    margin-top: 50px;
-    display:flex;
-    justify-content:center;
-    align-items: center;
-`
-
-const ContentTextWrapper = styled.div`
+const ThemeAllWapper = styled.div`
     width:310px;
     height:auto;
-    margin-top:20px;
+    display: flex;
+    justify-content:center;
+    flex-wrap: wrap;
 `
 
+// 선택 버튼 fixed를 하기위한 div
 const ButtomFix = styled.div`
     position:fixed;
     bottom: 70px;
@@ -32,13 +19,9 @@ const ButtomFix = styled.div`
 
 
 const ThemeKinds = () =>{
-
-    return(<>
-    <ThemeImgBox><img src ={Internet}/></ThemeImgBox>
-    <Margin width="390" height="32"/>
-    <Typography mainTitle>엔터테인먼트</Typography>
-    <ContentTextWrapper><Typography contentText color="darkGray">엔터테인먼트 PBV는 사용자에게 오락, 방송 등에 적합한 환경을 제공합니다. 훌륭한 사운드와 4K UHD가 마련된 공간을 만나보세요.</Typography></ContentTextWrapper>
-    <ButtomFix><Button bgColor="mainRed">선택</Button></ButtomFix>
+    return(<>   
+        <ThemeAllWapper><SimpleSlider/></ThemeAllWapper>
+        <ButtomFix><Button bgColor="mainRed">선택</Button></ButtomFix>
     </>)
 }
 
