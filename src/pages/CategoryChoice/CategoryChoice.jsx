ac06b577af7e24651137cfc7b36927a252dd7e64
sean <sean2337@naver.com>
2023-02-13 11:22:55 +0900

    feat:CategoryChoice 기본 틀 완성

---
+++ src/pages/CategoryChoice/CategoryChoice.jsx
@@ -0,0 +1,25 @@
+import MobileLayout from "../../component/MobileLayout/MobileLayout";
+import Typography from "../../component/Typography/Typhography";
+import styled from "styled-components";
+import ThemeKinds from "./component/ThemeKinds";
+
+const TitleWrapper = styled.div`
+    width:100%;
+    height:70px;
+    margin-top: 64px;
+    padding-left: 40px;
+`
+
+const CategoryChoice = () =>{
+
+
+    return(<>
+    <MobileLayout bar>
+        <TitleWrapper><Typography mainTitle>필요한 테마 한가지를 <br/>골라주세요</Typography></TitleWrapper>
+        <ThemeKinds/>
+    
+    </MobileLayout>
+    </>)
+}
+
+export default CategoryChoice;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import CategoryChoice from "../pages/CategoryChoice/CategoryChoice";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/CategoryChoice" element={<CategoryChoice />} />
     </Routes>
   </BrowserRouter>
 );
