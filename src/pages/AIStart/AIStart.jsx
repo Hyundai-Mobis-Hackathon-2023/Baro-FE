ddd061d47e223d7715a64bf2c1cab76bb29d6b5f
DongRyeong Lee <dr11291129@gmail.com>
2023-02-14 00:05:38 +0900

    feat(ALStart): ai 시작 페이지 작성

---
+++ src/pages/AIStart/AIStart.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+import MobileLayout from "../../component/MobileLayout/MobileLayout";
+import Margin from "../../component/Margin/Margin";
+import Lottie from "lottie-react";
+import lottie1 from "../../assets/svgs/lottie.json";
+import { ReactComponent as Loading } from "../../assets/svgs/loading.svg";
+import Typography from "../../component/Typography/Typhography";
+import Button from "../../component/Button/Button";
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: linear-gradient(
+    190.59deg,
+    rgba(151, 91, 227, 0.6) 0.59%,
+    rgba(151, 91, 227, 0.813753) 26.98%,
+    #7000ff 97.46%
+  );
+`;
+
+const TextWrapper = styled.div`
+  width: 100%;
+  height: 70px;
+  padding-left: 39px;
+`;
+
+const ButtonWrapper = styled.div`
+  position: fixed;
+  bottom: 74px;
+`;
+
+const AIStart = () => {
+  return (
+    <MobileLayout>
+      <Background>
+        <Margin height="241" />
+        <Loading />
+        <Margin height="174" />
+        <TextWrapper>
+          <Typography mainTitle color="white">
+            AI와 함께 구상해요
+          </Typography>
+          <Margin height="20" />
+          <Typography aiContentText color="white">
+            openAI로 이루어진 채팅봇과 함께 당신에게
+            <br />
+            알맞는 현대 PBV를 찾아보세요.
+            <br />
+            알맞은 사용법을 여기에 적어줍니다.
+          </Typography>
+        </TextWrapper>
+        <ButtonWrapper>
+          <Button bgColor="white" color="black">
+            AI 만나기
+          </Button>
+        </ButtonWrapper>
+      </Background>
+    </MobileLayout>
+  );
+};
+
+export default AIStart;
+
+/*
+<Margin height="100" />
+        <Lottie animationData={lottie1} />
+*/
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import CategoryChoice from "../pages/CategoryChoice/CategoryChoice";
 import DialNumberChoice from "../pages/DialNumberChoice/DialNumberChoice";
+import AIStart from "../pages/AIStart/AIStart";
+
 import SignUp from "../pages/SignUp/SignUp";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/AIStart" element={<AIStart />} />
       <Route exact path="/SignUp" element={<SignUp />} />
       <Route exact path="/CategoryChoice" element={<CategoryChoice />} />
       <Route exact path="/DialNumberChoice" element={<DialNumberChoice />} />
