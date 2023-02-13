0950bb59e4241138b14e0908151b177941ba485b
pakxe <pigkill40@naver.com>
2023-02-13 17:10:08 +0900

    refactor(Routes): 불필요한 exact prop 제거

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import CategoryChoice from "../pages/CategoryChoice/CategoryChoice";
-import SignUp from "../pages/SignUp/SignUp";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import CategoryChoice from '../pages/CategoryChoice/CategoryChoice';
+import SignUp from '../pages/SignUp/SignUp';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/SignUp" element={<SignUp />} />
-      <Route exact path="/CategoryChoice" element={<CategoryChoice />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/SignUp' element={<SignUp />} />
+      <Route path='/CategoryChoice' element={<CategoryChoice />} />
     </Routes>
   </BrowserRouter>
 );
