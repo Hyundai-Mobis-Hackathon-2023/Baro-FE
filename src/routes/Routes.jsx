f9926e40720ad1e94d4d0e6cbfcbd7b103f02386
Sihyeon Joo <sihyeonjoo@Sihyeonui-MacBookAir.local>
2023-02-15 13:52:01 +0900

    feat:sign-up->login 파일변경

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CategoryChoice from '../pages/CategoryChoice/CategoryChoice';
-import SignUp from '../pages/SignUp/SignUp';
+import Login from '../pages/Login/Login';
 import Temp from '../pages/Temp/Temp';
 import AIStart from '../pages/AIStart/AIStart';
 import DialNumberChoice from '../pages/DialNumberChoice/DialNumberChoice';
@@ -12,7 +12,7 @@ const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Start />} />
-      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/login' element={<Login />} />
       <Route path='/category-choice' element={<CategoryChoice />} />
       <Route path='/temp' element={<Temp />} />
       <Route path='/preset/:selectedCategory' element={<Preset />} />
