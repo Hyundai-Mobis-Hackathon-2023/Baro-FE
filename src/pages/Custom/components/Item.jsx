d5eff6688be7bafafc3ab242547fec2ae77f0049
pakxe <pigkill40@naver.com>
2023-02-16 00:55:38 +0900

    feat: 아이템, 아이템 컨테이너, 카테고리 컴포넌트 완성

---
+++ src/pages/Custom/components/Item.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import theme from '../../../assets/theme/Theme';
+import Typography from '../../../component/Typography/Typhography';
+
+const ItemContainer = styled.div`
+  width: 154px;
+  height: 183px;
+  margin: 14px 0;
+
+  p {
+    padding-left: 5px;
+    padding-top: 9px;
+  }
+`;
+
+const Photo = styled.div`
+  width: 154px;
+  height: 154px;
+  border-radius: 24px;
+  border: 1px solid ${theme.colors.blueGray};
+  overflow: hidden;
+
+  display: flex;
+  justify-content: center;
+
+  img {
+    position: relative;
+    top: -50px;
+    height: 250px;
+  }
+`;
+
+const Item = ({ name, photo, onClick, id }) => {
+  return (
+    <ItemContainer id={id} onClick={onClick}>
+      <Photo>
+        <img src={require(`../images/${photo}.png`)} />
+      </Photo>
+      <Typography contentText>{name}</Typography>
+    </ItemContainer>
+  );
+};
+
+export default Item;
