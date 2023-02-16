e233325961b784439e74a5b85d6f60512018db3f
DongRyeong Lee <dr11291129@gmail.com>
2023-02-16 15:16:40 +0900

    chore(Routes): 라우터 링크 일부 수정

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,31 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CategoryChoice from '../pages/CategoryChoice/CategoryChoice';
-import Login from '../pages/Login/Login';
-import Temp from '../pages/Temp/Temp';
-import AIStart from '../pages/AIStart/AIStart';
-import DialNumberChoice from '../pages/DialNumberChoice/DialNumberChoice';
-import NewUser from '../pages/NewUser/NewUser';
-import Start from '../pages/Start/Start';
-import Preset from '../pages/Preset/Preset';
-import MakeStart from '../pages/MakeStart/MakeStart';
-import InstalledWidget from '../pages/InstalledWidget/InstalledWidget';
-import Custom from '../pages/Custom/Custom';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CategoryChoice from "../pages/CategoryChoice/CategoryChoice";
+import Login from "../pages/Login/Login";
+import Temp from "../pages/Temp/Temp";
+import AIStart from "../pages/AIStart/AIStart";
+import DialNumberChoice from "../pages/DialNumberChoice/DialNumberChoice";
+import NewUser from "../pages/NewUser/NewUser";
+import Start from "../pages/Start/Start";
+import Preset from "../pages/Preset/Preset";
+import MakeStart from "../pages/MakeStart/MakeStart";
+import InstalledWidget from "../pages/InstalledWidget/InstalledWidget";
+import Custom from "../pages/Custom/Custom";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Start />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/category-choice' element={<CategoryChoice />} />
-      <Route path='/temp' element={<Temp />} />
-      <Route path='/preset/:selectedCategory' element={<Preset />} />
-      <Route path='/number_choice' element={<DialNumberChoice />} />
-      <Route path='/ai-start' element={<AIStart />} />
-      <Route path='/new-user' element={<NewUser />} />
-      <Route path='/make-start' element={<MakeStart />} />
-      <Route path='/installed-widget' element={<InstalledWidget/>} />
-      <Route path='/custom' element={<Custom />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/start" element={<Start />} />
+      <Route path="/category-choice" element={<CategoryChoice />} />
+      <Route path="/temp" element={<Temp />} />
+      <Route path="/preset/:selectedCategory" element={<Preset />} />
+      <Route path="/number_choice" element={<DialNumberChoice />} />
+      <Route path="/ai-start" element={<AIStart />} />
+      <Route path="/new-user" element={<NewUser />} />
+      <Route path="/make-start" element={<MakeStart />} />
+      <Route path="/installed-widget" element={<InstalledWidget />} />
+      <Route path="/custom" element={<Custom />} />
     </Routes>
   </BrowserRouter>
 );
