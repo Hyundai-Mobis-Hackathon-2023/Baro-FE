c00dbb4346a329b338b05437249ffe08546d6b36
DongRyeong Lee <dr11291129@gmail.com>
2023-02-18 01:38:39 +0900

    chore: 중간 과정 저장

---
+++ src/pages/AIChat/component/AIInput.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useRef } from "react";
+import styled from "styled-components";
+import Button from "../../../component/Button/Button";
+import Margin from "../../../component/Margin/Margin";
+import Typography from "../../../component/Typography/Typhography";
+import { motion } from "framer-motion";
+
+const StyledMotion = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const IconWrapper = styled.div`
+  width: 326px;
+  ${(props) => props.theme.flex.flexCenter}
+  justify-content: space-between;
+`;
+
+const StyledTextarea = styled.textarea`
+  width: 320px;
+  ${(props) => props.theme.text.buttonText}
+  text-align: center;
+  word-wrap: break-word;
+  word-break: keep-all;
+  border: none;
+  overflow: hidden;
+  caret-color: #8a5bdf;
+  background: none;
+  resize: none;
+  ::placeholder {
+    color: #e0dede;
+  }
+`;
+
+const AIInput = () => {
+  return (
+    <StyledMotion
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <IconWrapper></IconWrapper>
+      <Margin height="56" />
+      <Typography $buttonText>
+        안녕하세요. 00채팅봇입니다.
+        <br />
+        어떤 차를 원하시나요?
+      </Typography>
+      <Margin height="20" />
+      <StyledTextarea
+        autoFocus
+        ref={textRef}
+        placeholder="입력해주세요"
+        maxLength="30"
+        onChange={textChanged}
+        onKeyPress={checkEnterAndSpace}
+      />
+      <Margin height="200" />
+    </StyledMotion>
+  );
+};
+
+export default AIInput;
