2188954d3d0a738892064e4f5ff98a82c078622f
Sihyeon Joo <sihyeonjoo@Sihyeonui-MacBookAir.local>
2023-02-18 00:51:50 +0900

    design:install-widget창 UI 디자인 변경

--- src/pages/InstalledWidget/InstalledWidget.jsx
+++ src/pages/InstalledWidget/InstalledWidget.jsx
@@ -3,6 +3,7 @@ import MobileLayout from "../../component/MobileLayout/MobileLayout";
 import KindWidgets from "./component/KindWidgets";
 import Typography from "../../component/Typography/Typhography";
 import styled from "styled-components";
+import PurpleTalk from "./component/Purpletalk.png"
 import {GoPlus} from "react-icons/go"
 import { useState, useEffect } from "react";
 
@@ -18,13 +19,7 @@ const Title2 = styled.div`
     line-height: 31.03px;
     font-weight: 700;
 `
-const IntroText = styled.div`
-    font-family: 'pretendard';
-    font-size: 20px;
-    line-height: 23.87px;
-    font-weight: 500;
-    color: #FFFFFF;
-`
+
 const PlusButton = styled.button`
     width: 68px;
     height: 48px;
@@ -53,14 +48,6 @@ const TitleTextWrapper = styled.div`
     flex-wrap: wrap;
     align-content: center;
 `
-const IntroBox = styled.div`
-    width: 100%;
-    height: 995px;
-    background: linear-gradient(196.13deg, rgba(117, 40, 218, 0.6) 25.23%, rgba(119, 32, 234, 0.814) 30.13%, #7000FF 64.26%);
-    box-shadow: inset 0px 4px 2px rgba(0, 0, 0, 0.1);
-    border-radius: 64px;
-
-`
 const KindText = styled.div`
     font-family: 'pretendard';
     font-size: 20px;
@@ -74,10 +61,20 @@ const KindTextWrapper =styled.div`
     width: 100%;
     height: auto;
     display:flex;
-    margin-top: 20px;
-    margin-bottom: 10px;
+    margin-bottom: 38px;
 `
 
+const PurpleTalkWrapper = styled.img`
+    width: 392px;
+    height: 225px;
+    position: relative;
+    top: -60px;
+`
+
+const WidgetWrapper = styled.div`
+    position: relative;
+    top: -55px;
+`
 
 
 //값을 받을때, Widgets과 종류에 대한 값 두개씩을 줘야함.
@@ -120,17 +117,16 @@ const InstalledWidget = () =>{
             <PlusButton><GoPlus size="28" color="#9042EA"/></PlusButton>
         </TitleWrapper>
 
-       
-            <IntroText>기본 세팅은 다음과 같아요</IntroText>
-     
-        <KindTextWrapper>
-            <KindText style={{color: kind==="베이직" ?'#E53E3E':'#1B1313',borderBlockColor: kind==="베이직" ?'#E53E3E':'#1B1313'  }} onClick={basicClick}>베이직</KindText>
-            <KindText style={{color: kind==="커스텀" ?'#E53E3E':'#1B1313', borderBlockColor: kind==="커스텀" ?'#E53E3E':'#1B1313' }} onClick={customClick}>커스텀</KindText>
-        </KindTextWrapper>
+        <PurpleTalkWrapper src={PurpleTalk}></PurpleTalkWrapper>
+            
+        <WidgetWrapper>
+            <KindTextWrapper>
+                <KindText style={{color: kind==="베이직" ?'#E53E3E':'#1B1313',borderBlockColor: kind==="베이직" ?'#E53E3E':'#1B1313'  }} onClick={basicClick}>베이직</KindText>
+                <KindText style={{color: kind==="커스텀" ?'#E53E3E':'#1B1313', borderBlockColor: kind==="커스텀" ?'#E53E3E':'#1B1313' }} onClick={customClick}>커스텀</KindText>
+            </KindTextWrapper>
 
-        <CurrentWidgets current={kind}/>
-        
-    
+            <CurrentWidgets current={kind}/>
+        </WidgetWrapper>
 
     </MobileLayout></>)
 }
