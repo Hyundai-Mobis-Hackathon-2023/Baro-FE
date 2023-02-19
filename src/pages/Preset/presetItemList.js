4f97244abd2d09333a70ceebfbdcb476b1c017c5
pakxe <pigkill40@naver.com>
2023-02-19 17:43:56 +0900

    feat(presetItemList): Preset에 들어가는 아이템 목록 작성

---
+++ src/pages/Preset/presetItemList.js
@@ -0,0 +1,34 @@
+const presetItemList = [
+  {
+    id: 1,
+    name: 'office',
+    items: [0, 2, 5, 9, 10],
+  },
+  {
+    id: 2,
+    name: 'living',
+    items: [5, 10, 14, 16],
+  },
+  {
+    id: 3,
+    name: 'entertainment',
+    items: [2, 5, 10, 15, 17],
+  },
+  {
+    id: 4,
+    name: 'store',
+    items: [1, 3, 13],
+  },
+  {
+    id: 5,
+    name: 'medical',
+    items: [0, 4, 12],
+  },
+  {
+    id: 6,
+    name: 'transit',
+    items: [6, 7, 8],
+  },
+];
+
+export default presetItemList;
