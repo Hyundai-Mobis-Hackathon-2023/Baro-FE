ce9678b0ca89017ea6b14a28047beb14ad44068d
DongRyeong Lee <dr11291129@gmail.com>
2023-02-21 13:52:32 +0900

    feat(PurchaseEnd): 결제 완료 페이지 완성

--- src/pages/AIChat/component/AIResult.jsx
+++ src/pages/AIChat/component/AIResult.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import Margin from "../../../component/Margin/Margin";
 import Typography from "../../../component/Typography/Typhography";
--- src/pages/PurchaseEnd/PurchaseEnd.jsx
+++ src/pages/PurchaseEnd/PurchaseEnd.jsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import Lottie from "lottie-react";
+import Flex from "../../component/Flex/Flex";
 import MobileLayout from "../../component/MobileLayout/MobileLayout";
+import car from "../../assets/svgs/car.json";
+import success from "../../assets/svgs/success.json";
+import Typography from "../../component/Typography/Typhography";
+import Margin from "../../component/Margin/Margin";
+import { useNavigate } from "react-router-dom";
+
+const StyledFlex = styled(Flex)`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+`;
 
 const PurchaseEnd = () => {
-  return <MobileLayout color="mainRed"></MobileLayout>;
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(function () {
+      navigate("/");
+    }, 7000);
+  });
+
+  return (
+    <MobileLayout color="mainRed">
+      <StyledFlex flexCenter column>
+        <Lottie
+          animationData={car}
+          style={{
+            position: "relative",
+            top: "100px",
+          }}
+        />
+        <Typography
+          buttonText
+          color="white"
+          style={{
+            textAlign: "center",
+            position: "relative",
+            top: "-20px",
+            lineHeight: "30px",
+          }}
+        >
+          결제가 완료되었습니다.
+          <br />
+          잠시 후, 차량이 도착할 예정입니다.
+        </Typography>
+        <Lottie
+          style={{
+            width: "150px",
+            height: "150px",
+          }}
+          animationData={success}
+          loop={false}
+        />
+        <Margin height="100" />
+      </StyledFlex>
+    </MobileLayout>
+  );
 };
 
 export default PurchaseEnd;
